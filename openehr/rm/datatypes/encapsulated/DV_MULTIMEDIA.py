f13735199051a6755a750bb6a68e83853c3246d5
chrispess <chrispess@gmail.com>
2013-08-10 21:53:35 -0300

    pacote multimídia. acerto de comments

--- openehr/rm/datatypes/encapsulated/DV_MULTIMEDIA.py
+++ openehr/rm/datatypes/encapsulated/DV_MULTIMEDIA.py
@@ -32,3 +32,57 @@ class DV_MULTIMEDIA(DV_ENCAPSULATED):
     #para evitarcircularidade da especificação OpenEHR, que define thumbnail = DV_MULTIMEDIA,optou-se por utilizar
     #o tipo "Image"  da "python image library" ou PIL
     thumbnail = Image
+
+    # referencia do tipo uri para informações externas como arquivos, entrada de base de dados, etc
+    uri = DV_URI
+
+    #os dados atualmente encontrados na URI, se estiverem inline
+    data = []
+
+    #tamanho em bytes dos dados presentes ou referenciados pela uri
+    size = typeInt
+
+    #construtor
+    def __init__(self, *args):
+     if(len(args)==2):
+      self.media_type = args[0]
+      self.size = args[1]
+     elif(len(args)>=2):
+         self.media_type = args[0]
+         self.size = args[1]
+         #todo definir os atributos que interessam
+     else:pass
+
+    #computado a  partir do valor de uri_atribute. True se o dado estava serializado externamente.
+    # Uma cópia pode estar serializada internamente, o que faria "is_expanded" = True
+
+     def is_external(self):
+      if(self.uri <> None):
+          return True
+      else:return False
+
+
+
+      #computado a  partir do valor do atributo data. True se o dado estava serializado internamente no EHR.
+
+      def is_inline(self):
+       if(self.data <> None):
+          return True
+       else:return False
+
+     #computada a partir do valor de compression_algorith_atribute. True, se os dados estão serializados em formato comprimido
+     def is_compressed(self):
+         if(self.compression_algoritm <> None):
+             return True
+         else:return False
+
+
+     #computada a partir do atributo integrity_check_algorithm. True se uma checagem de integridade foi computada
+      def has_integrity_check(self):
+          if(self.integrity_check_algorithm <> None):
+              return True
+          else:return False
+
+
+
+
