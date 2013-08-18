606571e57efa2f1e9913b460ab280777653ba440
chrispess <chrispess@gmail.com>
2013-08-17 23:11:35 -0300

    pacote terminologias

--- openehr/rm/support/terminology/CODE_SET_ACCESS.py
+++ openehr/rm/support/terminology/CODE_SET_ACCESS.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'chrispess'
+
+from openehr.BASIC_TYPES import typeStr
+
+
+#define um  objeto que provê um acesso proxy a um "code_set"
+class CODE_SET_ACCESS():
+
+    #construtor
+    def __init__(self):
+        pass
+
+    #identificador externo a um code_set
+    id = typeStr
+
+
+
