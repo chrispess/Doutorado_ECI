9394e815e3d58461924e7a76153fb0e1efb1e2ad
chrispess <chrispess@gmail.com>
2013-08-17 22:50:42 -0300

    pacote terminologias

--- openehr/rm/support/terminology/OPENEHR_CODE_SET_IDENTIFIERS.py
+++ openehr/rm/support/terminology/OPENEHR_CODE_SET_IDENTIFIERS.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'chrispess'
+
+from openehr.BASIC_TYPES import typeStr
+
+#lista de identificadores para conjunto de códigos do "OpenEHR terminology"
+class OPENEHR_CODE_SET_IDENTIFIERS():
+
+    #construtor
+    def __init__(self):
+        pass
+
