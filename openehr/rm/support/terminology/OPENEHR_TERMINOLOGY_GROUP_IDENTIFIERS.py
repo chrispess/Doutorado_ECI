39dc80682d3bfd641d108d63752dfd196071d7b3
chrispess <chrispess@gmail.com>
2013-08-17 22:14:16 -0300

    pacote terminologias

--- openehr/rm/support/terminology/OPENEHR_TERMINOLOGY_GROUP_IDENTIFIERS.py
+++ openehr/rm/support/terminology/OPENEHR_TERMINOLOGY_GROUP_IDENTIFIERS.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 __author__ = 'chrispess'
+
+from openehr.BASIC_TYPES import typeStr
+
+class OPENEHR_TERMINOLOGY_GROUP_IDENTIFIERS():
+
+    #construtor
+    def __init__(self):
+        pass
+
+
+    #
+    Terminology_id = typeStr
+
+
