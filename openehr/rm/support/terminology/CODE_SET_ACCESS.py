de58046f17b3a55293c7dbe662a23e5a758cb5b7
chrispess <chrispess@gmail.com>
2013-08-17 23:18:15 -0300

    pacote terminologias

--- openehr/rm/support/terminology/CODE_SET_ACCESS.py
+++ openehr/rm/support/terminology/CODE_SET_ACCESS.py
@@ -3,8 +3,9 @@ __author__ = 'chrispess'
 
 from openehr.BASIC_TYPES import typeStr
 
-
+#no  original da especificação OpenEHR trata-se de uma interface
 #define um  objeto que provê um acesso proxy a um "code_set"
+
 class CODE_SET_ACCESS():
 
     #construtor
@@ -13,6 +14,27 @@ class CODE_SET_ACCESS():
 
     #identificador externo a um code_set
     id = typeStr
+
+    #identificador externo de um code_set
+    def id(self, id = typeStr):
+        self.id = id
+
+    #retorna todos ops códigos do code_set
+    def all_codes(self):
+        pass
+
+    #retorna True se o code_set contém "a_lang"
+    def has_lang(self):
+        pass
+
+    # retorna True se o code_set contém "a_code"
+    def has_code(self):
+        pass
+
+
+
+
+
 
 
 
