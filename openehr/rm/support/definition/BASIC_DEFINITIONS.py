1dd67549e3aa1d18d6032a84463950e5bd33230f
chrispess <chrispess@gmail.com>
2013-08-18 00:18:38 -0300

    pacote terminologias

---
+++ openehr/rm/support/definition/BASIC_DEFINITIONS.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+__author__ = 'chrispess'
+
+from openehr.BASIC_TYPES import typeStr
+
+#define valores de constantes globais
+class BASIC_DEFINITIONS():
+
+     #construtor
+     def  __init__(self):
+         pass
+
+
+     CR = typeStr
+     CR = "\015"
+
+     LF = typeStr
+     LF = "\012"
+
+
+     def get_CR(self):
+         return self.CR
+
+     def get_LF(self):
+         return self.LF
+
+
+
+
+
--- openehr/rm/support/definition/OPENEHR_DEFINITIONS.py
+++ openehr/rm/support/definition/OPENEHR_DEFINITIONS.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'chrispess'
+
+from openehr.rm.support.definition.BASIC_DEFINITIONS import BASIC_DEFINITIONS
+
+class OPENEHR_DEFINITIONS(BASIC_DEFINITIONS):
+
+    pass
